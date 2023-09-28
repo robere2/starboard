2fda8367fed5b5c657a681fc71c05dfa4940a478
Erik <7330807+robere2@users.noreply.github.com>
2023-09-27 20:57:12 -0400

    Move Hypixel content to bundled folder

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,8 @@ export * from './cache/InMemoryCache.ts';
 
 export * from './endpoint/Endpoint.ts';
 
-export * from './hypixel/HypixelService.ts';
-export * from './hypixel/HypixelEndpoint.ts';
+export * from './bundled/hypixel/HypixelService.ts';
+export * from './bundled/hypixel/HypixelEndpoint.ts';
 
 export * from './plugin/Plugin.ts';
 
