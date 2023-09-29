e072c513c67753bfb702d82e84386c64ad595fda
Erik <7330807+robere2@users.noreply.github.com>
2023-09-29 14:00:37 -0400

    Improve index.ts exports

---
+++ src/bundled/index.ts
@@ -0,0 +1,4 @@
+export * from './InMemoryCache.ts';
+
+export * from './hypixel/HypixelService.ts';
+export * from './hypixel/HypixelEndpoint.ts';
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,10 @@
 export * from './Starboard.ts'
 
 export * from './Cache.ts';
-export * from './bundled/InMemoryCache.ts';
-
 export * from './Endpoint.ts';
-
-export * from './bundled/hypixel/HypixelService.ts';
-export * from './bundled/hypixel/HypixelEndpoint.ts';
-
+export * from './Executor.ts';
 export * from './Plugin.ts';
-
 export * from './Service.ts';
 
-export * from './Executor.ts';
+export * from './bundled';
 
