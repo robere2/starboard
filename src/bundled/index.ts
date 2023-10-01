675002d71521eaca99b02cb1b6ec2a71d028408a
Erik <7330807+robere2@users.noreply.github.com>
2023-10-01 19:26:40 -0400

    Update exports

--- src/bundled/index.ts
+++ src/bundled/index.ts
@@ -1,4 +1,10 @@
+export * from './MojangAPI.ts';
+
 export * from './InMemoryCache.ts';
 
 export * from './hypixel/HypixelService.ts';
 export * from './hypixel/HypixelEndpoint.ts';
+export * from './hypixel/endpoints/HypixelPlayerEndpoint.ts';
+
+export * from './hypixel/api/HypixelAPI.ts';
+export * from './hypixel/api/HypixelPlayer.ts';
